d694264e60ce07f8495f49da965b7fe85e72036e
StijnU-Devoteam <stijn.uytterhoeven@devoteam.com>
2024-08-02 14:24:09 +0200

    generate external table code

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
-let hello = "hello"
-
-exports.hello = hello
+function generateCreateExternalTableSQL(table) {
+    tables.push(table)
+    const {name, format, uris } = table;
+    const uriList = uris.map(uri => `'${uri}'`).join(', ');
+  
+    const sql = `
+      CREATE EXTERNAL TABLE IF NOT EXISTS \`${name}\`
+      OPTIONS (
+        format = "${format}",
+        uris = [${uriList}]
+      );`
+      
+    return sql
+  }
+  
+  module.exports = { generateCreateExternalTableSQL };
